8e83c1dfde4fd1b0f4ec5b624442db62e7247092
Oriol Arbusi Abadal <oriol.abadal@mongodb.com>
2026-02-19 10:36:20 +0100

    ci and version constraint

--- modules/resource_policy/versions.tf
+++ modules/resource_policy/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 2.6" # TODO: CLOUDP-379763 bump after provider fixes are released
+      version = "~> 2.7"
     }
   }
 }
