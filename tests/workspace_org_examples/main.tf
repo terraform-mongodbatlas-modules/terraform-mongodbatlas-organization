197abb0c9df5597b965828b3e43d9fd8c8cfe3e3
Espen Albert <espen.albert@mongodb.com>
2026-02-27 11:15:19 +0100

    simplify workspace test to only use_existing_org

--- tests/workspace_org_examples/main.tf
+++ tests/workspace_org_examples/main.tf
@@ -4,47 +4,13 @@ terraform {
       source  = "mongodb/mongodbatlas"
       version = "~> 2.7"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
   required_version = ">= 1.9"
 }
 
 provider "mongodbatlas" {}
 
-variable "org_owner_id" {
+variable "existing_org_id" {
   type        = string
-  description = "Atlas user ID for org creation tests."
-  default     = ""
-}
-
-variable "org_name_prefix" {
-  type        = string
-  default     = "test-acc-tf-o-"
-  description = "org name prefix when auto-generating name."
-}
-
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-resource "mongodbatlas_organization" "this" {
-  name         = "${var.org_name_prefix}${random_string.suffix.id}"
-  org_owner_id = var.org_owner_id
-  description  = "atlas org used in org module tests"
-  role_names   = ["ORG_OWNER"]
-}
-
-provider "mongodbatlas" {
-  alias       = "existing_org"
-  public_key  = mongodbatlas_organization.this.public_key
-  private_key = mongodbatlas_organization.this.private_key
-}
-
-locals {
-  existing_org_id = mongodbatlas_organization.this.org_id
+  description = "Existing organization ID for existing-org example."
 }
