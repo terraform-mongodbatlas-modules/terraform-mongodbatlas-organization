88b4c1f1f2adb1541226f78536514fcb7edf43ff
Espen Albert <EspenAlbert@users.noreply.github.com>
2026-02-27 12:49:56 +0100

    chore: Adds workspace test for existing-org (#21)
    * chore: sync sdlc from cb6a7b70
    
    * chore: Update code-health workflow to include MONGODB_ATLAS_ORG_ID secret and adjust snapshot test commands
    
    * add plan snapshot tests for existing-org
    
    * chore: Add organization-specific commands for Terraform operations
    
    * chore: Refactor pre-release tests to use organization-specific commands for applying and destroying examples
    
    * fix lint errors
    
    * chore: fmt
    
    * refactor: use custom org for pre-release tests
    
    ---------
    
    Co-authored-by: path-sync[bot] <path-sync[bot]@users.noreply.github.com>

---
+++ tests/workspace_org_examples/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "~> 2.7"
+    }
+  }
+  required_version = ">= 1.9"
+}
+
+provider "mongodbatlas" {}
+
+# tflint-ignore: terraform_unused_declarations
+variable "existing_org_id" {
+  type        = string
+  description = "Existing organization ID for existing-org example."
+}
