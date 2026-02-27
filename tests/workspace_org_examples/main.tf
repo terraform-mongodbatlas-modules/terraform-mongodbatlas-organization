e8fc9f7e95ab5b0abbd4aa62b033be8b03766cd3
Espen Albert <espen.albert@mongodb.com>
2026-02-27 08:13:57 +0100

    chore: add random provider and organization resource for testing

--- tests/workspace_org_examples/main.tf
+++ tests/workspace_org_examples/main.tf
@@ -4,15 +4,15 @@ terraform {
       source  = "mongodb/mongodbatlas"
       version = "~> 2.7"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
   required_version = ">= 1.9"
 }
 
 provider "mongodbatlas" {}
-variable "org_id" {
-  type        = string
-  description = "Existing organization ID for existing-org and import tests."
-}
 
 variable "org_owner_id" {
   type        = string
@@ -20,7 +20,31 @@ variable "org_owner_id" {
   default     = ""
 }
 
+variable "org_name_prefix" {
+  type        = string
+  default     = "test-acc-tf-o-"
+  description = "org name prefix when auto-generating name."
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "mongodbatlas_organization" "this" {
+  name         = "${var.org_name_prefix}${random_string.suffix.id}"
+  org_owner_id = var.org_owner_id
+  description  = "atlas org used in org module tests"
+  role_names   = ["ORG_OWNER"]
+}
+
+provider "mongodbatlas" {
+  alias       = "existing_org"
+  public_key  = mongodbatlas_organization.this.public_key
+  private_key = mongodbatlas_organization.this.private_key
+}
 
 locals {
-  existing_org_id = var.org_id
+  existing_org_id = mongodbatlas_organization.this.org_id
 }
